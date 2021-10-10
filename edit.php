8f8c30eae166f9366960a585f406d7009831dbf5
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-10 22:45:16 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,5 +1,5 @@
 <?php
-// including the database connection file
+
 include_once("config.php");
  
 if(isset($_POST['update']))
@@ -10,7 +10,7 @@ if(isset($_POST['update']))
     $age=$_POST['age'];
     $email=$_POST['email'];    
     
-    // checking empty fields
+   
     if(empty($name) || empty($age) || empty($email)) {            
         if(empty($name)) {
             echo "<font color='red'>Name field is empty.</font><br/>";
@@ -24,19 +24,18 @@ if(isset($_POST['update']))
             echo "<font color='red'>Email field is empty.</font><br/>";
         }        
     } else {    
-        //updating the table
-        $result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
         
-        //redirectig to the display page. In our case, it is index.php
+        $result = mysqli_query($mysqli, "UPDATE users SET name='$name',age='$age',email='$email' WHERE id=$id");
+              
         header("Location: index.php");
     }
 }
 ?>
 <?php
-//getting id from url
+
 $id = $_GET['id'];
  
-//selecting data associated with this particular id
+
 $result = mysqli_query($mysqli, "SELECT * FROM users WHERE id=$id");
  
 while($res = mysqli_fetch_array($result))
