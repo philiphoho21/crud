72737b908c40208364cb68bbad9419fe0f80fb8a
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-10 22:44:14 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,13 +1,7 @@
 <?php
-//including the database connection file
+
 include("config.php");
- 
-//getting id of the data from url
-$id = $_GET['id'];
- 
-//deleting the row from table
-$result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
- 
-//redirecting to the display page (index.php in our case)
-header("Location:index.php");
+ $id = $_GET['id'];
+ $result = mysqli_query($mysqli, "DELETE FROM users WHERE id=$id");
+ header("Location:index.php");
 ?>
