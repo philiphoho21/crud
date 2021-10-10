93ba968e221488dc68f5cecede887f223545ea65
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-10 22:43:08 +0800

    Update add.php

--- add.php
+++ add.php
@@ -5,7 +5,7 @@
  
 <body>
 <?php
-//including the database connection file
+
 include_once("config.php");
  
 if(isset($_POST['Submit'])) {    
@@ -15,7 +15,7 @@ if(isset($_POST['Submit'])) {
        
         $result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
         
-        //display success message
+        
         echo "<font color='green'>Data added successfully.";
         echo "<br/><a href='index.php'>View Result</a>";
 }
