3c741a7ea46f7de78b2bec9ce66a39ca5a8b0ea2
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-10 22:45:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
-//including the database connection file
+
 include_once("config.php");
  
-//fetching data in descending order (lastest entry first)
-//$result = mysql_query("SELECT * FROM users ORDER BY id DESC"); // mysql_query is deprecated
 $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
 ?>
  
@@ -23,8 +21,7 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
             <td>Action</td>
         </tr>
         <?php 
-        //while($res = mysql_fetch_array($result)) { // mysql_fetch_array is deprecated, we need to use mysqli_fetch_array 
-        while($res = mysqli_fetch_array($result)) {         
+             while($res = mysqli_fetch_array($result)) {         
             echo "<tr>";
             echo "<td>".$res['name']."</td>";
             echo "<td>".$res['age']."</td>";
