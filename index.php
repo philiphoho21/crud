7347de39ea5bc0098d9c4d99bafccb4912b489e8
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-11 13:33:29 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
 include_once("config.php");
  
-$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // using mysqli_query instead
+$result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC");
 ?>
  
 <html>
@@ -17,14 +16,17 @@ $result = mysqli_query($mysqli, "SELECT * FROM users ORDER BY id DESC"); // usin
         <tr>
             <td>Name</td>
             <td>Age</td>
+             <td>Address</td>
             <td>Email</td>
             <td>Action</td>
         </tr>
         <?php 
-             while($res = mysqli_fetch_array($result)) {         
+        
+        while($res = mysqli_fetch_array($result)) {         
             echo "<tr>";
             echo "<td>".$res['name']."</td>";
             echo "<td>".$res['age']."</td>";
+            echo "<td>".$res['address']."</td>";  
             echo "<td>".$res['email']."</td>";    
             echo "<td><a href=\"edit.php?id=$res[id]\">Edit</a> | <a href=\"delete.php?id=$res[id]\" onClick=\"return confirm('Are you sure you want to delete?')\">Delete</a></td>";        
         }
