fa8760c24cef82985b281cf45c0a31d4c59a1319
philiphoho21 <48244769+philiphoho21@users.noreply.github.com>
2021-10-11 13:30:28 +0800

    Update add.php

--- add.php
+++ add.php
@@ -11,11 +11,12 @@ include_once("config.php");
 if(isset($_POST['Submit'])) {    
     $name = $_POST['name'];
     $age = $_POST['age'];
+    $address = $_POST['address'];
     $email = $_POST['email'];
        
-        $result = mysqli_query($mysqli, "INSERT INTO users(name,age,email) VALUES('$name','$age','$email')");
+        $result = mysqli_query($mysqli, "INSERT INTO users(name,age,address,email) VALUES('$name','$age','$address','$email')");
         
-        
+   
         echo "<font color='green'>Data added successfully.";
         echo "<br/><a href='index.php'>View Result</a>";
 }
@@ -29,6 +30,7 @@ if(isset($_POST['Submit'])) {
 </head>
  
 <body>
+    <center>
     <a href="index.php">Home</a>
     <br/><br/>
  
@@ -42,6 +44,10 @@ if(isset($_POST['Submit'])) {
                 <td>Age</td>
                 <td><input type="text" name="age" required=""></td>
             </tr>
+             <tr> 
+                <td>Address</td>
+                <td><input type="text" name="address" required=""></td>
+            </tr>
             <tr> 
                 <td>Email</td>
                 <td><input type="text" name="email" required=""></td>
@@ -52,5 +58,6 @@ if(isset($_POST['Submit'])) {
             </tr>
         </table>
     </form>
+</center>
 </body>
 </html>
